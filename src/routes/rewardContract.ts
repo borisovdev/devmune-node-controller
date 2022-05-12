e49ff490578aea13e2a51c369e09cefc7d19015a
Alexandr <borisovdeveloper@gmail.com>
2022-05-12 20:32:39 +0000

    Added: send test signed transaction

--- src/routes/rewardContract.ts
+++ src/routes/rewardContract.ts
@@ -4,15 +4,16 @@ import { AbiItem } from "web3-utils";
 
 const router = express.Router();
 
-router.get(`/`, (req, res) => {
-  web3.eth.net.getId((err, id) => {
-    if (err) {
-      res.status(500).json({ message: err.message });
-      return;
-    }
+router.get(`/`, async (req, res) => {
+  res.status(200).json({ network_id: 42000 });
+  // web3.eth.net.getId((err, id) => {
+  //   if (err) {
+  //     res.status(500).json({ message: err.message });
+  //     return;
+  //   }
 
-    res.status(200).json({ network_id: 1000 });
-  });
+  //   res.status(200).json({ network_id: 42000 });
+  // });
 });
 
 router.get(`/test-http`, async (req, res) => {
@@ -21,18 +22,27 @@ router.get(`/test-http`, async (req, res) => {
 
     const testHttpContract = new web3.eth.Contract(
       abi.default as AbiItem[],
-      "0x7320ADf9A5f6021157410154ee1B0E0E0DBc8524",
-      {
-        from: "0x329163a9af542BbfAD345aa11e43035c3d682Ee7",
-      }
+      "0x3b2172C076e6A0C99627D21472Cc911620713A0B"
     );
+    const testHttpContractMethodTestAbi = testHttpContract.methods.requestTestData().encodeABI();
 
-    const result = await testHttpContract.methods
-      .requestHelloOracle()
-      .call({
-        from: "0x329163a9af542BbfAD345aa11e43035c3d682Ee7",
+    /**
+     * @todo may use @ethereumjs/tx for signing transactions
+     */
+    const signedTestHttpContractMethodCallTransaction = await web3.eth.accounts.signTransaction({
+      to: testHttpContract.options.address,
+      data: testHttpContractMethodTestAbi,
+      gas: 200000,
+    }, "d6243f1e1582ac961236eb280e5ee6ee899480164c2f58332e4600634f2ad467");
+
+    // @ts-ignore
+    web3.eth.sendSignedTransaction(signedTestHttpContractMethodCallTransaction.rawTransaction)
+      .on("receipt", (receipt: any) => {
+        res.status(200).json(receipt);
       })
-      res.json(result);
+      .on("error", (err: Error) => {
+        res.status(500).json({message: err.message});
+      })
   } catch (err) {
     // @ts-ignore
     res.status(500).json({ message: `${err.message}` });
